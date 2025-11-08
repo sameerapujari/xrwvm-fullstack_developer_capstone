42b20972fc34cd2f0a7491e1f544e18f7ecee282
Sameera <sameerapujari.15@gmail.com>
2025-11-08 13:56:09 -0500

    task5

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealer/:id" element={<Dealer/>} />
+        <Route path="/postreview/:id" element={<PostReview/>} />
       </Routes>
     </Router>
   );
